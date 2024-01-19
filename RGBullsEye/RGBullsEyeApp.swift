436c89fdce4763b458a196958f6fd18cca123762
Fuse <fusethnd@gmail.com>
2024-01-19 14:42:31 +0700

    Initial Commit

---
+++ RGBullsEye/RGBullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBullsEyeApp.swift
+//  RGBullsEye
+//
+//  Created by Thanadon Boontawee on 19/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct RGBullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
